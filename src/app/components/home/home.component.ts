443fae041bd7b7dce865da49c514b6de604a5490
CasasolaCano <casasolacontact@gmail.com>
2021-02-09 23:46:34 +0100

    Notes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,3 +1,4 @@
+//Import necesarioi para usar las peticiones http
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
@@ -13,7 +14,7 @@ export class HomeComponent implements OnInit {
   countries: any[] = [];
 
   constructor(private http:HttpClient) {
-
+    //Petición http get de angular para recoger datos de paises.
     this.http.get('https://restcountries.eu/rest/v2/lang/es').subscribe((data:any) => {
       this.countries = data;
       console.log(data);
