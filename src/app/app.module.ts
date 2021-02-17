9812368e100570042077425e5589b56783430118
CasasolaCano <casasolacontact@gmail.com>
2021-02-17 15:57:48 +0100

    Font awesome angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 //Para trabajar con peticiones http necesitamos importarlo de angular, todos los import
 //que contengan la palabra module, tienen que ser colocados dentro del @NgModule en imports:
@@ -19,6 +21,7 @@ import { ROUTES } from './app.routes';
 //Pipes
 import { NoimagePipe } from './pipes/noimage.pipe';
 import { CardsComponent } from './components/cards/cards.component';
+import { LoadingComponent } from './components/shared/loading/loading.component';
 
 
 
@@ -30,12 +33,14 @@ import { CardsComponent } from './components/cards/cards.component';
     ArtistComponent,
     NavbarComponent,
     NoimagePipe,
-    CardsComponent
+    CardsComponent,
+    LoadingComponent
   ],
   imports: [    //Todos los import que contengan module deben ser implementados aquí
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(ROUTES, {useHash:true})
+    RouterModule.forRoot(ROUTES, {useHash:true}),
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
